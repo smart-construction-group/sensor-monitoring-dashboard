b37c17875d16e71461847d96c5e2a5a3a1ed4143
Amin AzGol <golestani76@yahoo.com>
2022-12-05 22:25:17 +0330

    add dynamic 3d heatmap location

--- src/pages/HeatMap3D.vue
+++ src/pages/HeatMap3D.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="frame-wrapper">
-    <iframe src="http://141.11.246.83:8282" frameborder="0"></iframe>
+    {{ heatmap3DSource }}
+    <iframe :src="heatmap3DSource" frameborder="0"></iframe>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      heatmap3DSource:
+        window.location.protocol + "//" + window.location.hostname + ":8282",
+    };
+  },
+};
 </script>
 
 <style>
-.frame-wrapper{
-    height: 95vh
+.frame-wrapper {
+  height: 95vh;
 }
-.frame-wrapper iframe{
-    width: 100%;
-    height: 100%;
+.frame-wrapper iframe {
+  width: 100%;
+  height: 100%;
 }
 </style>
