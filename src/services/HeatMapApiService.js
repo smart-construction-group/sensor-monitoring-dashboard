2610cfc7b3d07443beb7d55bcb77ab12e758bb7a
Amin AzGol <golestani76@yahoo.com>
2022-12-07 20:18:16 +0330

    change the server static url to dynamic url

--- src/services/HeatMapApiService.js
+++ src/services/HeatMapApiService.js
@@ -1,30 +1,31 @@
 
 import axios from 'axios'
 import { formatDate } from '../utils/Utils'
-const APIUrl = "http://141.11.246.83:9090"
+const APIUrl = "http://" + window.location.hostname + ":9090"
 // const APIUrl = "http://localhost:9090"
 
 
 export async function fetchHeatMapData(config) {
-    if(config.from instanceof Date) config.from = formatDate(config.from)
-    if(config.to instanceof Date) config.to = formatDate(config.to)
+    console.log(APIUrl)
+    if (config.from instanceof Date) config.from = formatDate(config.from)
+    if (config.to instanceof Date) config.to = formatDate(config.to)
 
-    var res = await axios.get(APIUrl+`/sensors?type=${config.type}&from=${config.from}&to=${config.to}&interval=${config.interval}`)
-    return {tableData: Object.keys(res.data), data: res.data}
+    var res = await axios.get(APIUrl + `/sensors?type=${config.type}&from=${config.from}&to=${config.to}&interval=${config.interval}`)
+    return { tableData: Object.keys(res.data), data: res.data }
 }
 
-function getMockData(config){
-    Date.prototype.addDays = function(days) {
+function getMockData(config) {
+    Date.prototype.addDays = function (days) {
         var date = new Date(this.valueOf());
         date.setDate(date.getDate() + days);
         return date;
     }
-    
+
     function getDates(startDate, stopDate) {
         var dateArray = new Array();
         var currentDate = startDate;
         while (currentDate <= stopDate) {
-            dateArray.push(new Date (currentDate));
+            dateArray.push(new Date(currentDate));
             currentDate = currentDate.addDays(1);
         }
         return dateArray;
@@ -33,18 +34,18 @@ function getMockData(config){
     var days = getDates(config.from, config.to)
     var data = {}
     var tableData = []
-    for(let i in days){
+    for (let i in days) {
         var day = days[i].toISOString().split('T')[0]
         tableData.push(day)
         data[day] = []
-        for(var j=0; j < parseInt(Math.random()*100); j++){
+        for (var j = 0; j < parseInt(Math.random() * 100); j++) {
             data[day].push({
-                device_id: "x"+i,
+                device_id: "x" + i,
                 x: Math.random() * 500,
                 y: Math.random() * 500,
                 value: Math.random() * 25
             })
         }
     }
-    return {data, tableData}
+    return { data, tableData }
 }
